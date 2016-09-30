bb4eb8f307a24dc43ebb9857c5974be173b13887
Björn Ricks <bjoern.ricks@gmail.com>
2016-09-30 18:30:37 +0200

    Route to login if not connected to daap server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 
-import App from './app';
+import {Daap} from 'daap.js';
+
+import App, {daap} from './app';
 import Login from './login';
 import Main from './main';
 
 import './index.css';
 
+const is_connected = (next_state, replace) => {
+  if (daap.status < Daap.Status.HasRevision) {
+    replace({
+      pathname: '/login',
+      state: {
+        next: next_state.location.pathname
+      }
+    });
+  }
+}
+
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route component={App}>
       <Route path="/login" component={Login}/>
-      <Route path="/" component={Main}/>
+      <Route path="/" component={Main} onEnter={is_connected}/>
     </Route>
   </Router>,
   document.getElementById('root')
