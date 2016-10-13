4c66b5f9cc81c852d71200fce483f742a933b285
Björn Ricks <bjoern.ricks@gmail.com>
2016-10-13 19:10:04 +0200

    Add Player component
    In future this player will be improved a lot. E.g. by using
    https://github.com/goldfire/howler.js

---
+++ src/player.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export class Player extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.playSong = this.playSong.bind(this);
+  }
+
+  playSong(song) {
+    console.log('Start to playing song');
+    this.audio.setAttribute('type', 'audio/' + song.format);
+    this.audio.setAttribute('src', song.stream_url);
+    this.audio.play();
+  }
+
+  render() {
+    return (
+      <div className="player">
+        <audio ref={ref => this.audio = ref} controls>
+          Your browser doesn't support audio.
+        </audio>
+      </div>
+    );
+  }
+}
+
+export default Player;
+
+// vim: set ts=2 sw=2 tw=80:
